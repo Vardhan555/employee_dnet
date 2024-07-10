8c77470c0c38e0f028af4579c7c866c4ca05a550
Vardhan555 <63648088+Vardhan555@users.noreply.github.com>
2024-07-10 18:07:56 -0500

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using employee.Data;
+using employee.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace employee.Controllers
+{
+	public class EmployeeController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		public EmployeeController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		//GET: /<controller>/
+		public IActionResult Index()
+		{
+			var employees = _context.Employees.ToList();
+			return View(employees);
+		}
+		public IActionResult Create()
+		{
+            ViewData["GenderList"] = new SelectList(new List<string> { "Male", "Female", "Other" });
+            return View();
+        }
+
+		[HttpPost]
+		public IActionResult Create(Employee employee)
+		{
+			if (ModelState.IsValid)
+			{
+				
+				_context.Employees.Add(employee);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+            }
+            ViewData["GenderList"] = new SelectList(new List<string> { "Male", "Female", "Other" });
+            return View();
+		}
+
+		public IActionResult Edit(int id)
+		{
+			var item = _context.Employees.Find(id);
+			if(item == null)
+			{
+				return NotFound();
+			}
+            ViewData["GenderList"] = new SelectList(new List<string> { "Male", "Female", "Other" });
+            return View(item);
+		}
+		[HttpPost]
+		public IActionResult Edit(int id, Employee emp)
+		{
+			if(id != emp.id)
+			{
+				return NotFound();
+			}
+			if (ModelState.IsValid)
+			{
+				_context.Update(emp);
+				_context.SaveChanges();
+			}
+            ViewData["GenderList"] = new SelectList(new List<string> { "Male", "Female", "Other" });
+            return View(emp);
+		}
+
+		public IActionResult Delete(int id)
+		{
+            var item = _context.Employees.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenderList"] = new SelectList(new List<string> { "Male", "Female", "Other" });
+            return View(item);
+        }
+		[HttpPost]
+		public IActionResult Delete(int id,Employee employee)
+		{
+			if(employee == null)
+			{
+				return NotFound();
+			}
+			if (ModelState.IsValid)
+			{
+				_context.Employees.Remove(employee);
+				_context.SaveChanges();
+			}
+            ViewData["GenderList"] = new SelectList(new List<string> { "Male", "Female", "Other" });
+
+            return RedirectToAction(nameof(Index));
+		}
+	}
+}
+
---
+++ Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using employee.Data;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+
+namespace employee.Models
+{
+
+    public class EmployeeContext : ApplicationDbContext
+    {
+        public EmployeeContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+	{
+		public int id { get; set; }
+		public string? name { get; set; }
+        [Required]
+        public string? email { get; set; }
+        [Required]
+        public string? City { get; set; }
+        [Required]
+        public string? Gender { get; set; }
+    }
+}
+
